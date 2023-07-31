24fc5c60f27f3b52b42489d3de78a5648f1a454e
Nephat Muchiri <muchirinephat5@gmail.com>
2023-07-31 12:18:45 +0300

    too lazy to insert comment

---
+++ src/main/java/com/neph/main/JavaPdfGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.neph.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPdfGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaPdfGeneratorApplication.class, args);
+    }
+
+}
